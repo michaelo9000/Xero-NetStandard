6c6ae50ed5e7559ed1c68bc132fd01188a42cca7
Jenks Guo <41350731+jenksguo@users.noreply.github.com>
2020-04-27 17:03:42 +1000

    Revert "Added namespace for Accounting & Assets Models"

--- Xero.NetStandard.OAuth2/Api/AssetApi.cs
+++ Xero.NetStandard.OAuth2/Api/AssetApi.cs
@@ -16,7 +16,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Mime;
 using Xero.NetStandard.OAuth2.Client;
-using Xero.NetStandard.OAuth2.Model.Asset;
+using Xero.NetStandard.OAuth2.Model;
 
 namespace Xero.NetStandard.OAuth2.Api
 {
